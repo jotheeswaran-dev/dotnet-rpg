2384eba9c53f825f2f3f6b2a2a23cef21a3ab759
jotheeswaran <jotheeswaran03@outlook.com>
2021-08-08 14:44:17 +0530

    Created releationships b/w Users & Characters

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace dotnet_rpg.Models
 {
     public class User
@@ -6,6 +8,7 @@ namespace dotnet_rpg.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public List<Character> Characters { get; set; }
 
     }
 }
