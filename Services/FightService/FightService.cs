d6ba032b44cacaf78d1b038eba89524d8a20deec
jotheeswaran <jotheeswaran03@outlook.com>
2021-08-17 16:22:18 +0530

    Implementing characters fight using skills

--- Services/FightService/FightService.cs
+++ Services/FightService/FightService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using dotnet_rpg.Data;
 using dotnet_rpg.Dtos.Fight;
@@ -13,6 +14,59 @@ namespace dotnet_rpg.Services.FightService
         public FightService(DataContext context)
         {
             _context = context;
+        }
+
+        public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request)
+        {
+            var response = new ServiceResponse<AttackResultDto>();
+            try
+            {
+                var attacker = await _context.Characters
+                    .Include(x => x.Skills)
+                    .FirstOrDefaultAsync(x => x.Id == request.AttackerId);
+
+                var opponent = await _context.Characters
+                    .FirstOrDefaultAsync(x => x.Id == request.OpponentId);
+
+                var skill = attacker.Skills.FirstOrDefault(x => x.Id == request.SkillId);
+
+                if (skill == null)
+                {
+                    response.Success = false;
+                    response.Message = $"{attacker.Name} doesn't know this skill";
+                    return response;
+                }
+
+                int damage = skill.Damage + (new Random().Next(attacker.HitPoints));
+                damage -= new Random().Next(opponent.Defense);
+
+                if (damage > 0)
+                {
+                    opponent.HitPoints -= damage;
+                }
+
+                if (opponent.HitPoints <= 0)
+                {
+                    response.Message = $"{opponent.Name} has been defeated";
+                }
+
+                await _context.SaveChangesAsync();
+
+                response.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHP = attacker.HitPoints,
+                    Opponent = opponent.Name,
+                    OpponentHP = opponent.HitPoints,
+                    Damage = damage
+                };
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
         }
 
         public async Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
